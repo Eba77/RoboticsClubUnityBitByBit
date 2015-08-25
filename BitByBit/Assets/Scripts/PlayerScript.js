cb24f65ef65a066b2942f2c1a254b7584696c6c0
Eba7 <e.bailey.andrew@gmail.com>
2015-08-24 19:25:35 -0700

    Now things are coming along

---
+++ BitByBit/Assets/Scripts/PlayerScript.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
+		this.GetComponent.<Rigidbody2D>().velocity = Vector2(-6,0);
+	}
+	if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
+		this.GetComponent.<Rigidbody2D>().velocity = Vector2(6,0);
+	}
+	if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
+		this.GetComponent.<Rigidbody2D>().velocity = Vector2(0,6);
+	}
+	if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
+		this.GetComponent.<Rigidbody2D>().velocity = Vector2(0,-6);
+	}
+}
