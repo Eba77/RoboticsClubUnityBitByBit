38f8532276488a733e394a4d1a0275de8fea083d
Eba7 <e.bailey.andrew@gmail.com>
2015-08-25 16:39:21 -0700

    Started working on the player

--- BitByBit/Assets/Scripts/PlayerScript.js
+++ BitByBit/Assets/Scripts/PlayerScript.js
@@ -1,5 +1,7 @@
 ﻿#pragma strict
 
+public static var DEBUG = true;
+
 //stats
 public static var attack: int;
 public static var maxHealth: int;
@@ -11,6 +13,15 @@ public static var speed: int;
 public static var bitsToSpend: int;
 public static var bitsSpent: int[]; 
 	//0 - sprite bits; 1 - atk; 2 - hp; 3 - def; 4 - spd; 
+	
+//world status
+public static var levelCode: int;
+public static var spawnLevelCode: int; //what the level code will be set to if you die.
+public static var spawnPosition: Vector2;
+
+//stat keeping
+public static var timesDied: int;
+public static var levelsEntered: int;
 
 function Start () {
 	bitsToSpend = 0;
@@ -23,19 +34,62 @@ function Start () {
 	health = maxHealth;
 	defense = 0+bitsSpent[3];
 	speed = 6+bitsSpent[4];
+	
+	levelCode = -1;
+	spawnLevelCode = -1;
+	spawnPosition = Vector2(0,0);
+	
+	//stat keepers
+	timesDied = 0;
+	levelsEntered = 0;
 }
 
 function Update () {
+	//movement
+	var rigid = this.GetComponent.<Rigidbody2D>();
 	if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
-		this.GetComponent.<Rigidbody2D>().velocity = Vector2(-speed,0);
+		rigid.velocity = Vector2(-speed,0);
 	}
 	if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
-		this.GetComponent.<Rigidbody2D>().velocity = Vector2(speed,0);
+		rigid.velocity = Vector2(speed,0);
 	}
 	if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
-		this.GetComponent.<Rigidbody2D>().velocity = Vector2(0,6);
+		rigid.velocity = Vector2(0,6);
 	}
 	if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
-		this.GetComponent.<Rigidbody2D>().velocity = Vector2(0,-6);
+		rigid.velocity = Vector2(0,-6);
 	}
+	//status updates
+	if (health<=0) die();
+}
+
+function die() {
+	timesDied = 0;
+	health = maxHealth;
+	loadLevel(spawnLevelCode);
+	this.GetComponent.<Transform>().position = spawnPosition;
+}
+
+function loadLevel(code:int) {
+	var position = this.GetComponent.<Transform>().position;
+	switch (code) {
+		case -1:
+			position = Vector2(0,0);
+		break;
+		default:
+			if (position.x>0) {
+				position = Vector2(-5,0);
+			}
+			else {
+				position = Vector2(5,0);
+			}
+		break;
+	}
+	
+	levelCode = code;
+}
+
+function takeDamage(damage:int) {
+	health-=damage;
+	if (DEBUG) Debug.Log("Ow: " + health + "/" + maxHealth);
 }
