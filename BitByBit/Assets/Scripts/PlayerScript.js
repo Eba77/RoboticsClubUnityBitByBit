033a4a476e5d21c7f93b7190119f98e032f21314
Eba7 <e.bailey.andrew@gmail.com>
2015-08-25 16:19:38 -0700

    Started adding stats

--- BitByBit/Assets/Scripts/PlayerScript.js
+++ BitByBit/Assets/Scripts/PlayerScript.js
@@ -1,15 +1,36 @@
 ﻿#pragma strict
 
-function Start () {
+//stats
+public static var attack: int;
+public static var maxHealth: int;
+public static var health: int;
+public static var defense: int;
+public static var speed: int;
 
+//bits
+public static var bitsToSpend: int;
+public static var bitsSpent: int[]; 
+	//0 - sprite bits; 1 - atk; 2 - hp; 3 - def; 4 - spd; 
+
+function Start () {
+	bitsToSpend = 0;
+	bitsSpent = new int[5];
+	for (var i = 0;i<bitsSpent.length;i++) {
+		bitsSpent[i] = 0;
+	}
+	attack = 5+bitsSpent[1];
+	maxHealth = 5+bitsSpent[2];
+	health = maxHealth;
+	defense = 0+bitsSpent[3];
+	speed = 6+bitsSpent[4];
 }
 
 function Update () {
 	if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
-		this.GetComponent.<Rigidbody2D>().velocity = Vector2(-6,0);
+		this.GetComponent.<Rigidbody2D>().velocity = Vector2(-speed,0);
 	}
 	if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
-		this.GetComponent.<Rigidbody2D>().velocity = Vector2(6,0);
+		this.GetComponent.<Rigidbody2D>().velocity = Vector2(speed,0);
 	}
 	if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
 		this.GetComponent.<Rigidbody2D>().velocity = Vector2(0,6);
